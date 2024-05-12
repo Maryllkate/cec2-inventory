e43921cecbd82beab83fb94bdf1534cb37c3d3b9
Maryll Driz <marylldriz@Marylls-MacBook-Pro.local>
2024-05-12 17:29:42 +0800

    fixed add stock issue

--- Backend/controller/purchaseStock.js
+++ Backend/controller/purchaseStock.js
@@ -1,28 +1,3 @@
-// const Purchase = require("../models/purchase");
-// const Product = require("../models/product");
-
-// const purchaseStock = async (productID, purchaseStockData) => {
-//   // Updating Purchase stock
-//   try {
-//     const myProductData = await Product.findOne({ _id: productID });
-//     let myUpdatedStock = myProductData.stock + purchaseStockData;
-//     console.log(myUpdatedStock);
-
-//     const PurchaseStock = await Product.findByIdAndUpdate(
-//       { _id: productID },
-//       {
-//         stock: myUpdatedStock,
-//       },
-//       { new: true }
-//     );
-//     console.log(PurchaseStock);
-//   } catch (error) {
-//     console.error("Error updating Purchase stock ", error);
-//   }
-// };
-
-// module.exports = purchaseStock;
-
 const Purchase = require("../models/purchase");
 const Product = require("../models/product");
 
